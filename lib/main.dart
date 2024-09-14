d0a86184b9fecd1abfb84d76775723016554789e
Mabrouk <abdelrahmanmabrouk89.2@gmail.com>
2024-09-14 20:12:02 +0300

    Uplode project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MasarakApp());
+}
+
+class MasarakApp extends StatelessWidget {
+  const MasarakApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+     // home: const MyHomePage(),
+    );
+  }
+}
+
