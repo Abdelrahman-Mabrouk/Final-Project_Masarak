5684441961605710daa2283a45a6b903fc71c76d
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2024-10-11 00:25:11 +0300

    Update metro_route_finder.dart

--- lib/Map/metro_route_finder.dart
+++ lib/Map/metro_route_finder.dart
@@ -1,26 +1,8 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class MetroRouteFinder extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Metro Stations App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MetroStationsScreen(),
-    );
-  }
-}
 
-class MetroStationsScreen extends StatefulWidget {
-  @override
-  _MetroStationsScreenState createState() => _MetroStationsScreenState();
-}
-
-class _MetroStationsScreenState extends State<MetroStationsScreen> {
+class MetroRouteFinder  {
   List stations = [];
   String? startStation;
   String? endStation;
@@ -32,19 +14,14 @@ class _MetroStationsScreenState extends State<MetroStationsScreen> {
     'السادات': [1, 2]
   };
 
-  @override
-  void initState() {
-    super.initState();
+  MetroRouteFinder(){
     loadStations();
   }
-
   // قراءة ملف JSON للمحطات
   Future<void> loadStations() async {
     String data = await rootBundle.loadString('assets/jsonFiles/metroLinesData.json');
 
-    setState(() {
-      stations = jsonDecode(data);
-    });
+    stations = jsonDecode(data);
   }
 
   // دالة لحساب المحطات بين خطين
@@ -94,108 +71,15 @@ class _MetroStationsScreenState extends State<MetroStationsScreen> {
           inRange = !inRange;
         } else if (inRange) {
           result.add(station['name']);
-          print("-------------------station['name']------------------------------");
-          print(station['name']);
+
         }
       }
     }
     if(result[0]==endStation){result= result.reversed.toList();result.removeAt(0);}
-    print("-------------------------------------------------");
-    print(start);
-    print(end);
-    print("-------------------------------------------------");
+
 
     return result;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Metro Route Finder'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            DropdownButton<String>(
-              hint: Text("اختر محطة البداية"),
-              value: startStation,
-              onChanged: (newValue) {
-                setState(() {
-                  startStation = newValue;
 
-                });
-              },
-              items: stations.map<DropdownMenuItem<String>>((station) {
-                return DropdownMenuItem<String>(
-                  value: station['name'],  // تعديل المفتاح إلى 'name'
-                  child: Text(station['name']),  // تعديل المفتاح إلى 'name'
-                );
-              }).toList(),
-            ),
-
-            DropdownButton<String>(
-              hint: Text("اختر محطة الوصول"),
-              value: endStation,
-              onChanged: (newValue) {
-                setState(() {
-                  endStation = newValue;
-                });
-              },
-              items: stations.map<DropdownMenuItem<String>>((station) {
-                return DropdownMenuItem<String>(
-                  value: station['name'],  // تعديل المفتاح إلى 'name'
-                  child: Text(station['name']),  // تعديل المفتاح إلى 'name'
-                );
-              }).toList(),
-            ),
-
-            ElevatedButton(
-              onPressed: () {
-                if (startStation != null && endStation != null) {
-                  if (startStation == endStation) {
-                    // عرض رسالة خطأ إذا كانت محطة البداية هي نفسها محطة النهاية
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text("خطأ"),
-                          content: Text("محطة البداية لا يمكن أن تكون هي نفسها محطة النهاية!"),
-                          actions: <Widget>[
-                            TextButton(
-                              child: Text("حسناً"),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  } else {
-                    // استمر في العملية إذا كانت محطتي البداية والنهاية مختلفتين
-                    setState(() {
-                      routeStations = getStationsBetween(startStation!, endStation!);
-                    });
-                  }
-                }
-              },
-              child: Text("عرض المسار"),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: routeStations.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(routeStations[index]),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
